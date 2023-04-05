dfe28502a016f05e4ae8f0eab4acd7536e459d58
agbleze <lagbleze@st.ug.edu.gh>
2023-04-05 23:02:45 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -3,6 +3,55 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+#%%
+
+df_g7sec1 = pd.read_spss(path='g7sec1.sav')
+
+#%%
+
+health = pd.read_spss(path='g7sec3b_Health_Insurance.sav')
+
+#%%
+
+g7sec7 = pd.read_spss(path='g7sec7.sav')
+
+#%%
+
+g7sec7j = pd.read_spss(path='g7sec7j.sav')
+
+
+#g7sec7j_sel = g7sec7j[[]]
+
+#%%
+
+df_g7sec1.columns
+
+#%%
+health.columns
+
+#%%
+
+g7sec7.columns
+
+#%%
+
+g7sec7j.columns
+
+
+#%%
+
+df_g7sec1['s1q6'].value_counts()
+
+
+#%% 
+df_g7sec1['s1q9'].count()
+
+#%%
+
+## used 'phid' as individuals in household
+
+# select columns of interest for each dataset
+# join data together on phid
 
 
 
@@ -10,3 +59,7 @@ import matplotlib.pyplot as plt
 
 
 
+
+
+
+# %%
