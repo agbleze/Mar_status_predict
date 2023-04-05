440dc62f4740d1626864f28d528a2789f203a8f7
agbleze <lagbleze@st.ug.edu.gh>
2023-04-05 23:52:32 +0200

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -19,6 +19,9 @@ g7sec7 = pd.read_spss(path='g7sec7.sav')
 
 g7sec7j = pd.read_spss(path='g7sec7j.sav')
 
+#%%
+
+g7sec2 = pd.read_spss(path='g7sec2.sav')
 
 #g7sec7j_sel = g7sec7j[[]]
 
@@ -54,9 +57,29 @@ df_g7sec1['s1q9'].count()
 # join data together on phid
 
 
+#%%
+
+df_sel = df_g7sec1[['phid', 'hid', 'clust', 'nh', 'pid', 's1q2', 's1q5y', 
+                     's1q14', 's1q18', 's1q22', 's1q10'
+                ]]
+
+#%%
+
+g7sec2[[]]
 
 
+#%%
+## s1q22 Number of months away from houshold
+## s1q22 father live in household  <<  14 Does (NAME’S) father live in this household?
+## s1q1b educational level <<  What is the highest level of education (NAME) has attained?
 
+
+## s1q2 sex of individual
+## s1q18  Mother live in household  <<  18 Does (NAME’S) mother live in this household?
+
+## s1q5y Age in years
+
+## s1q10 What is (NAME’S) religious denomination?
 
 
 
