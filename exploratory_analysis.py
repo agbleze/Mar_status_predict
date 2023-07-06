67bad878a6238d788b9d37df8111014b60a52912
agbleze <lagbleze@st.ug.edu.gh>
2023-07-06 15:50:16 +0200

    Create exploratory_analysis.py

---
+++ exploratory_analysis.py
@@ -0,0 +1,23 @@
+"""
+Before proceeding with it, one cannot resist addressing a frequently 
+asked question concerning when it should be undertaken. 
+Yes, it is always at the first stages but lets talk about its focus.
+
+Very often the question is asked;
+
+Should the dataset be split before undertaking the exploratory analysis?
+
+Should exploratory analysis be undertaken on all the dataset or only the training dataset?
+
+Well, these two questions are driving at the same thing from the same angle.
+
+The answers are Yes and No with solid arguemnt for both so that it all truns down to be 
+where you see things from base on your situation. I will argue for each point to present 
+the views based on which you decide.
+
+
+Yes, the dataset should be split before exploratory analysis!
+Exploratory analysis should be based on only training data!
+
+
+"""
